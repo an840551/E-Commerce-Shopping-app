facded6c270fe40709bf082d833c181ddb66da11
Anjali Namdeo <68951262+an840551@users.noreply.github.com>
2022-08-22 20:57:57 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-960,/pub/media/catalog/product/a/7/a741931NYKFF_LSDY0003_1.jpg?rnd=20200526195200",
+        title: "SUMMER Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/f/6/f6d1c30FKE11486_1.jpg?rnd=20200526195200",
+        title: "WINTER Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcfled"
+    },
+    {id: 3,
+        img: "https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-550,/pub/media/catalog/product/7/4/74dcb81ANVI-2-CS011_1.jpg?rnd=20200526195200",
+        title: "FESTIVAL Sale",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4"}
+];
+
+export const categories = [
+    {
+        id:1,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSC47Ez7nihhO1VhCQJX1xdd3iyu92RtFk7_TV-dwmCGVGurYlfiywguZERXNoZoLfgisirX_XaxA&usqp=CAc",
+        title: "SHIRT STYLE"
+    },
+    {
+        id:2,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQchxSgssxxNpetfhyExUqVg7qogtO_KLiTkBnKyN_lz6QCajIUAzYza6773lZKlwivaSDCY97k-gIUUTkwYYE1LZhrKcN6iSf27bTeoQD81vKSAldwvnjvWeM&usqp=CAc",
+        title: "LOUNGEWEAR LOVE"
+    },
+    {
+        id:3,
+        img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/summer-jackets-1651861038.png?crop=1.00xw:0.752xh;0,0.178xh&resize=980:*",
+        title: "LIGHT JACKETS"
+    }
+];
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/SS22/AFpage/Rec/SBC/Exploremore/english/4-SBC-Kids-Fashion_04._CB626340508_.jpg"
+    },
+    {
+        id: 2,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/SBC/Kids/SBC-Kids_03._CB623869110_.gif"
+    },
+    {
+        id: 3,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/Newseason/12_._SY530_QL85_.jpg"
+    },
+    {
+        id: 4,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/SBC/Kids/SBC-Kids_06._CB623869110_.gif"
+    },
+    {
+        id: 5,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/Newseason/11_._SY530_QL85_.jpg"
+    },
+    {
+        id: 6,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/Newseason/7_0._SY530_QL85_.jpg"
+    },
+    {
+        id: 7,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/Newseason/3._SY530_QL85_.jpg"
+    },
+    {
+        id: 8,
+        img: "https://images-na.ssl-images-amazon.com/images/G/31/img22/Fashion/Flip/AFpage/Redesign/Newseason/4._SY530_QL85_.jpg"
+    }
+];
+
